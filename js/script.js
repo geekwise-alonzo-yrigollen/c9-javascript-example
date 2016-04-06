aec05a356bb631d77886c6768a8bb0267ec6970c
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-06 19:59:46 +0000

    jsphoto

--- js/script.js
+++ js/script.js
@@ -1,18 +1,43 @@
-var div_tag;
-var div_tag = document.createElement('div');
+var photo_container;
+var img_urls = new Array;
 
-var p_tag;
-var p_tag = document.createElement('p');
+var add_images = function(number_of_images){
+   var image;
+   for(var i=0; i< number_of_images; i++){
+       img_urls.push('/images/profile_photo_' + (i + 1) + '.jpg');
+       image = document.createElement('img');
+       image.setAttribute('src', img_urls[i]);
+       document.body.appendChild(image);
+   };
+};
 
-document.addEventListener('DOMContentLoaded',function(event){
-   
-   document.body.appendChild(div_tag);
-   
-   div_tag.appendChild(p_tag);
-   
-    console.log()
-  
+document.addEventListener('DOMContentLoaded', function(event){
+   add_images(68);
    
    
-    
+   
+   
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
