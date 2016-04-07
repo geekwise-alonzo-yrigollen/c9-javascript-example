c7bc3318dca863b51c5c06bff06df0973a247e28
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-07 15:11:49 +0000

    work

--- js/script.js
+++ js/script.js
@@ -1,40 +1,44 @@
 var photo_container;
+
 var img_urls = new Array;
 
 var add_images = function(number_of_images){
-   var image;
-   for(var i=0; i< number_of_images; i++){
-       img_urls.push('/images/profile_photo_' + (i + 1) + '.jpg');
-       image = document.createElement('img');
-       image.setAttribute('src', img_urls[i]);
-       document.body.appendChild(image);
-   };
+    
+    var image;
+    
+    for(var i=0; i< number_of_images; i++){
+        img_urls.push('images/profile_photo_' + (i + 1) + '.jpg');
+        image = document.createElement('img');
+        image.setAttribute('src', img_urls[i]);
+        image.style.width = number_of_images;
+        
+        document.body.appendChild(image);
+        
+        
+        
+         image.addEventListener('mouseover', function(event){
+             
+             this.style.transform = "rotateZ(180deg)";
+             this.style.transition = '1s';
+             
+         });
+         
+         image.addEventListener('click', function(event){
+             
+             
+              this.style.transform = "rotateZ(360deg)";
+              this.style.transition = '1s';
+             
+         }); 
+        
+    }
 };
 
 document.addEventListener('DOMContentLoaded', function(event){
-   add_images(68);
-   
-   
-   // deddebudg
-   
-   
+    
+    add_images(68,'25%');
+    
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
