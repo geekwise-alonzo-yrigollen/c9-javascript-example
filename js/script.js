07cb4fa03cbbf0b01773670f582db9f88200587d
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-08 18:21:51 +0000

    tackle js

--- js/script.js
+++ js/script.js
@@ -18,29 +18,39 @@ var add_images = function(number_of_images){
         
          image.addEventListener('mouseover', function(event){
              
-             this.style.transform = "rotateZ(180deg)";
-             this.style.transition = '1s';
+             this.style.transform = "rotateY(360deg)";
+             
+             this.style.flip = "rotateY(360deg)";
+             
+             this.style.transition = '1.1s';
              
          });
          
          image.addEventListener('click', function(event){
              
              
-              this.style.transform = "rotateZ(360deg)";
-              this.style.transition = '1s';
+              this.style.transform = "rotateZ(180deg)";
+              this.style.transition = '1.1s';
+              
              
+            
          }); 
         
     }
 };
 
+
+
+
+
 document.addEventListener('DOMContentLoaded', function(event){
     
-    add_images(68,'25%');
+    add_images(68,'5%');
+    
+    
+   
     
 });
-
-
 
 
 
