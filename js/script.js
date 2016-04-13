bb99e4b105df4447b51a8f226851d713515b34c3
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-13 00:25:04 +0000

    today

--- js/script.js
+++ js/script.js
@@ -1,21 +1,24 @@
-var http_request = new XMLHttpRequest();
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+  
+  var http_request = new XMLHttpRequest();
 http_request.open('GET','https://api.github.com',false);
 http_request.send(null);
 
-var github_data = JSON.parse(http_request.response);
-
-http_request = new XMLHttpRequest();
+var github_data = JSON.parse(http_request.response);http_request = new XMLHttpRequest();
 http_request.open('GET', github_data.emojis_url,false);
 http_request.send(null);
 
 var github_emojis = JSON.parse(http_request.response);
 
 for( i in github_emojis){
-    
-    
-    var image_container = document.createElement('img');
-    image_container.setAttribute('src','github_emojis[i]');
-    document.body.appendChild(image_container);
-    
-    
+ 
+  var image_container = document.createElement('img');
+  image_container.setAttribute('src',github_emojis[i]);
+  document.body.appendChild(image_container);
+ 
+ 
 };
+  
+});
