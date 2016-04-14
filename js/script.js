4f53b72e70b24159597fb4bc1a5251af0f93b799
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-14 05:49:51 +0000

    percent sign does allow for change

--- js/script.js
+++ js/script.js
@@ -1,53 +1,82 @@
 var github_emojis;
-var http_request = new XMLHttpRequest();
+
+var http_request = new XMLHttpRequest;
 http_request.open('GET','https://api.github.com/emojis',false);
-http_request.onreadystatechange = function(){
-  
-if(this.readyState === this.Done && this.status === 200){
-  
-  github_emojis = JSON.parse(http_request.response);
-  
-};  
-var get_randome_number = function(){
-  
-  var total_number = Math.round(Math.random()*total_number_of_emojis);
-  var emoji_name = Object.keys(github_emojis)[random_number];
-  
-  emoji_name = emoji_name.toString();
-  var emoji_url = github_emojis [emoji_name];
+
+http_request.onreadystatechange = function(event){	
+	if(this.readyState === this.DONE && this.status === 200){
+	
+		 github_emojis = JSON.parse(this.response);
+		
+	}
 };
 
-var git_emoji_urls=function(amount_of_urls){
-  
-  var total_amount_of_emojis = Object.keys(github_emojis).length;
-};
-for(var i=0;i<amount_of_urls; i++){
-  var emoji_name = Object.keys(github_emojis)[i];
-  emoji_name = emoji_name.toString();
-  var emoji_url = github_emojis[emoji_name];
-  
-  
-  
-  var image = document.createElement('img');
-  image.setAttribute('src', emoji_url);
-  document.body.appendChild(image);
-  
-  var count = 0;
-};
-// for(var keys in github_emojis){
-//   if(github_emojis.hasOwnProperty(keys)++count);
-// };
+var get_random_emoji = function(){
+    
+    var total_number_of_emojis = Object.keys(github_emojis).length;
+    var random_number = Math.round(Math.random()*total_number_of_emojis);
+    
+    var emoji_name = Object.keys(github_emojis)[random_number];
+    emoji_name = emoji_name.toString();
+    
+    var emoji_url = github_emojis[emoji_name];
+    return emoji_url;
+}
 
 
-// };
+
+
+var get_emoji_urls = function(amount_to_get){
+    
+        var total_number_of_emojis = Object.keys(github_emojis).length;
+        for(var i=0; i < amount_to_get; i++){
+            
+            var emoji_name = Object.keys(github_emojis)[i];
+            var emoji_url = github_emojis[emoji_name];
+            
+            emoji_name = emoji_name.toString();
+            
+         
+            var image = document.createElement('img');
+            image.addEventListener('click',function(event){
+                 
+               this.setAttribute('src',get_random_emoji()); 
+                
+            });
+            
+             image.addEventListener('mouseover',function(event){
+                 
+               this.style.opacity = '.9';
+            
+             });
+             
+             image.addEventListener('mouseout',function(event){
+                 
+               this.style.opacity = '.9';
+            
+             });
+            
+            
+            
+            
+            
+            image.setAttribute('src',emoji_url);
+
+            document.body.appendChild(image);
+
+        }
+        
+        
+}
+
+
+
+
 document.addEventListener('DOMContentLoaded',function(event){
-  
-  http_request.send(null);
-  get_emoji_urls(10);
+    
+   
+    http_request.send(null);
+    get_emoji_urls(10);
+    
+    
 });
-
-
-
-
-
-
