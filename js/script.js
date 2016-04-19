1bc7bddfda6ad9f01196ff7fa6b5081bb5fa36e6
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-19 17:19:36 +0000

    today js

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var input_box = document.createElement('input');
+var word_list = ["ben", "tell", "old", "due"];
+
+
+document.addEventListener('DOMContentLoaded',function(event){
+    
+   
+for(var i=0; i < word_list.length; i++){
+    
+    console.log(word_list[i]);
+    
+}
+    
+   document.body.appendChild(input_box);
+    
+});
+
