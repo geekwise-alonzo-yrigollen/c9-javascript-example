bc020c9c1f3201e710fb153fc91c94ece293daa2
alonzo <geekwise.alonzo.yrigollen@gmail.com>
2016-04-21 23:25:21 +0000

    today

--- js/script.js
+++ js/script.js
@@ -1,49 +1,24 @@
-var input_box = document.createElement('input');
-var word_list = ["ben", "tell", "old", "due"];
-word_list.push("see","hey","get");
-var random_number = Math.random()*word_list.length;
-
-
+var calculator = ['1','2','3','+','='];
+var input_box = document.createElement(input)
 document.addEventListener('DOMContentLoaded',function(event){
-    
-   document.body.appendChild(input_box);
 
 
-     input_box.addEventListener('keydown',function(event){
-         console.log(event.code);
-         console.log(event.code[3]);
-         if(event.keyCode === 13){
-             var element = document.createElement('p');
-             element.textContent = this.value;
-             //document.body.appendChild(element);
-         this.parentNode.insertBefore(element, this.nextSibling);
-             this.value = null;
-             
-        }
-    });
 
 
-    
+   for( var i = 0; i < calculator.length; i++){
    
-for(var i=0; i < word_list.length; i++){
-	
-	
-	
-	var p_element = document.createElement('p');
-	p_element.textContent = word_list[i];
-	document.body.appendChild(p_element);
-	
-	
-	p_element.addEventListener('click', function(event){
-	    
-	    this.style.backgroundColor = 'green';
-	    console.log(this);
-	    
-	});
+      var p = document.createElement('p');
+      p.setAttribute('id','p_' +i);
+      p.textContent = calculator[i];
+      
+      document.body.appendChild(p);
 
-}
+      console.log( calculator[i]);
+      
+   };
+   
+}); 
 
-    
-});
 
- 
+
+
